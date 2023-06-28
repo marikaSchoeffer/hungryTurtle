47daedfa60cf6163a9570bc2d2211c9c5b480cb4
marikaSchoeffer <schoeffer.marika@yahoo.de>
2023-06-28 12:35:08 +0200

    Installed MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-function App() {
+import { Box, Paper, Typography } from "@mui/material";
+
+
+export function App() {
   return (
-    <>
-      <h1>Hello Trutle!</h1>
-    </>
+    <Box>
+      <Paper>
+        <Typography>
+          Hello Trutle!
+        </Typography>
+        </Paper>
+    </Box>
   )
 }
 
-export default App
