62cb1ecca3fac90c0941c078fa64bfbd2efb3f46
marikaSchoeffer <schoeffer.marika@yahoo.de>
2023-09-06 08:51:25 +0200

    Changed style.ts

--- src/style.ts
+++ src/style.ts
@@ -3,7 +3,13 @@ import { styled } from "@mui/system";
 
 export const primaryColor = "#f705cb";
 export const pageSize = 9;
-export const StyledAlert = styled(Alert)(({ theme }) => ({
-  backgroundColor: "#fcecfc",
-  borderColor: primaryColor,
-}));
+export const StyledAlert = styled(Alert)(
+  (
+    {
+      /*theme*/
+    }
+  ) => ({
+    backgroundColor: "#fcecfc",
+    borderColor: primaryColor,
+  })
+);
